c09fdfba39c8127ef7ac4510257e795e49a12668
Austin Rebelo <000917638@mohawkcollege.ca>
2024-09-26 14:25:08 -0400

    Add opening

--- rebelo-austin-a1-choices/Program.cs
+++ rebelo-austin-a1-choices/Program.cs
@@ -1,3 +1,17 @@
 ﻿//Game Dev Foundations
 //Austin Rebelo
 //Assignment 1
+
+// Displays title and asks for a name
+Console.WriteLine("The Ultimate Short Quest\n");
+Console.WriteLine("Enter your name adventuer.");
+
+// Gets user input for a name and isplays it
+string name = Console.ReadLine();
+Console.WriteLine("Welcome" + name + "!\n");
+
+//
+Console.WriteLine("To begin, please select a class\n");
+Console.WriteLine("1. Warrior, equiped with a sword and shield");
+Console.WriteLine("2. Ranger, has a quiver containing normal and explosive arrows");
+Console.WriteLine("3. Mage, wields fire and ice magic");
