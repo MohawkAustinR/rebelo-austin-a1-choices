5ceaa0c0842a556223bcb94865a7bc2a919f8657
Austin Rebelo <000917638@mohawkcollege.ca>
2024-09-26 14:55:17 -0400

    Add start of warrior path

--- rebelo-austin-a1-choices/Program.cs
+++ rebelo-austin-a1-choices/Program.cs
@@ -6,12 +6,24 @@
 Console.WriteLine("The Ultimate Short Quest\n");
 Console.WriteLine("Enter your name adventuer.");
 
-// Gets user input for a name and isplays it
+// Gets user input for a name and displays it
 string name = Console.ReadLine();
-Console.WriteLine("Welcome" + name + "!\n");
+Console.WriteLine("Welcome " + name + "!\n");
 
-//
+// Ask user to select a class
 Console.WriteLine("To begin, please select a class\n");
 Console.WriteLine("1. Warrior, equiped with a sword and shield");
 Console.WriteLine("2. Ranger, has a quiver containing normal and explosive arrows");
 Console.WriteLine("3. Mage, wields fire and ice magic");
+int playerClass = int.Parse(Console.ReadLine());
+
+// If the user types "1", they play as a warrior
+if (playerClass == 1)
+{
+    Console.WriteLine();
+    Console.WriteLine("You are " + name + " the ranger. You are on a quest to defeat the almighty King of Frost! \n");
+    Console.WriteLine("The King of Frost readies himself! Prepare to attack");
+    Console.WriteLine("Actions\n");
+    Console.WriteLine("1. Sword");
+    Console.WriteLine("2. Shield");
+}
