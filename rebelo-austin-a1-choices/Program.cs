1e7e42495e66de127f74911b436764423c6f7d07
Austin Rebelo <000917638@mohawkcollege.ca>
2024-09-26 15:22:41 -0400

    Finish warrior path

--- rebelo-austin-a1-choices/Program.cs
+++ rebelo-austin-a1-choices/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Enter your name adventuer.");
 
 // Gets user input for a name and displays it
 string name = Console.ReadLine();
-Console.WriteLine("Welcome " + name + "!\n");
+Console.WriteLine($"Welcome,{name}!\n");
 
 // Ask user to select a class
 Console.WriteLine("To begin, please select a class\n");
@@ -21,9 +21,26 @@ int playerClass = int.Parse(Console.ReadLine());
 if (playerClass == 1)
 {
     Console.WriteLine();
-    Console.WriteLine("You are " + name + " the ranger. You are on a quest to defeat the almighty King of Frost! \n");
-    Console.WriteLine("The King of Frost readies himself! Prepare to attack");
+    Console.WriteLine($"You are {name}, the warrior. You are on a quest to defeat the almighty King of Frost!\nYou leave your village and journey the path to the kings fortress... and you've arrived!\n(With such a short walk you'd expect someone to have challenged the king already.)\n");
+    Console.WriteLine("The King of Frost readies himself! Prepare to attack.");
     Console.WriteLine("Actions\n");
     Console.WriteLine("1. Sword");
     Console.WriteLine("2. Shield");
+    int warriorAction = int.Parse(Console.ReadLine());
+
+    // If user types "1", they use a sword and win
+    if (warriorAction == 1)
+    {
+        Console.WriteLine();
+        Console.WriteLine("You charge at the king and plunge your sword through his chest!\nThe King of Frost falls, defeated in one stab... Well that was easy.\n");
+        Console.WriteLine("YOU WIN!");
+    }
+
+    // If user types "2", they use a shield and lose
+    else if (warriorAction == 2)
+    {
+        Console.WriteLine();
+        Console.WriteLine("You raise your shield, preparing for the kings attack.\nThe King of Frost summons ice spike from beneath you, rendering your shield useless.\nObviously it wouldn't be that simple to beat the king\n");
+        Console.WriteLine("YOU LOSE!");
+    }
 }
